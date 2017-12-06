b5fdfe0f7000191b232eeb74b5b610c041fa8577
Koen Kamman <kammankoen@gmail.com>
2017-12-06 23:25:07 +0100

    ServerUrl Fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  serverUrl: 'https://world-building-api.herokuapp.com/api/v1/'
+  serverUrl: 'https://world-building-api.herokuapp.com/api/v1'
 };
